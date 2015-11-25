3a07bcc4e36ac51f2e2c0444d73c19b9ee4f0bdb
Molly Waggett <mwaggett@alumni.reed.edu>
2015-11-25 11:04:51 -0800

    Allow user to add their own items to the checklist.

--- Checklists/ChecklistViewController.swift
+++ Checklists/ChecklistViewController.swift
@@ -8,7 +8,8 @@
 
 import UIKit
 
-class ChecklistViewController: UITableViewController {
+class ChecklistViewController: UITableViewController,
+                                        AddItemViewControllerDelegate {
     
     var items: [ChecklistItem]
     
@@ -88,16 +89,15 @@ class ChecklistViewController: UITableViewController {
         tableView.deleteRowsAtIndexPaths(indexPaths, withRowAnimation: .Automatic)
     }
     
-    @IBAction func addItem() {
-                    let newRowIndex = items.count
-                    let item = ChecklistItem()
-                    item.text = "I am a new row"
-                    item.checked = false
-                    items.append(item)
-                    
-                    let indexPath = NSIndexPath(forRow: newRowIndex, inSection: 0)
-                    let indexPaths = [indexPath]
-                    tableView.insertRowsAtIndexPaths(indexPaths, withRowAnimation: .Automatic)
+    override func prepareForSegue(segue: UIStoryboardSegue,
+                                    sender: AnyObject?) {
+        if segue.identifier == "AddItem" {
+            let navigationController = segue.destinationViewController
+                                                as! UINavigationController
+            let controller = navigationController.topViewController
+                                                as! AddItemViewController
+            controller.delegate = self
+        }
     }
     
     func configureCheckmarkForCell(cell: UITableViewCell,
@@ -115,5 +115,21 @@ class ChecklistViewController: UITableViewController {
         label.text = item.text
     } 
     
+    func addItemViewControllerDidCancel(controller: AddItemViewController) {
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    func addItemViewController(controller: AddItemViewController,
+                                    didFinishAddingItem item: ChecklistItem) {
+        let newRowIndex = items.count
+        
+        items.append(item)
+        
+        let indexPath = NSIndexPath(forRow: newRowIndex, inSection: 0)
+        let indexPaths = [indexPath]
+        tableView.insertRowsAtIndexPaths(indexPaths, withRowAnimation: .Automatic)
+        
+        dismissViewControllerAnimated(true, completion: nil)
+    }
 }
 
